6f27495e6ba3cd18da9a04edf3b5d7d1ae79df8b
VDarius-NYE <darovarga@gmail.com>
2026-05-11 10:51:48 +0200

    Category.java (kesz)

---
+++ src/main/java/hu/nye/taskmanager/model/Category.java
@@ -0,0 +1,31 @@
+package hu.nye.taskmanager.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String color;
+}
